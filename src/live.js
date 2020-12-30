bb2827d0e6b8a45e32fb2fcf1bbefa1f6d00f0d5
Totone <contact@totone.dev>
2020-12-30 18:39:02 +0100

    doc: a comment explains how to use the plugin in the script

--- src/live.js
+++ src/live.js
@@ -1,3 +1,55 @@
+/**
+ * Live Update plugin.
+ * Allows you to update separately each selected graph element.
+ * 
+ * Use "live.api" and "live.refresh" in the meta data of the diagram to configure 
+ * the plugin:
+ * 
+ *    - live.api: url prefix to request the distant API (optional, see below).
+ *    - live.refresh: interval between 2 updates, set in seconds (optional, 
+ *      default is 10s).
+ *    - Both are stored as attributes for the graph element with id = 0.
+ * 
+ * If you want a graph element becomes live updatable, you must add at least one 
+ * of the following properties to the element (right click > "Edit Data" or 
+ * CTRL+M):
+ * 
+ *    - live.text: updates element text node.
+ *    - live.style: updates element style.
+ *    - live.property.<PROPERTY_NAME>: updates element <PROPERTY_NAME> value.
+ *        Example: "live.property.fillOpacity" updates "Fill Opacity" element 
+ *        property.
+ * 
+ * The value for each live propertiy is the url used to get the requested resource.
+ * The value can be an absolute path (ex: https://my.api/path/to/resource) or a 
+ * relative path: in this case, stored value is concatenated with live.api prefix 
+ * to build the request full url.
+ * 
+ * The APIs receiving requests MUST respond only a string corresponding to the 
+ * updated value for the requested attribute:
+ *    - "live.text" endpoint only returns updated displayed text.
+ *    - "live.style" endpoint returns full updated element style.
+ *    - "live.property.<PROPERTY_NAME>" endpoint only returns the updated value 
+ *      for corresponding element property.
+ * 
+ * To handle live update behaviour, use the buttons stored in interface toolbar:
+ * 
+ *    - ⏺︎ (Play) : Starts the plugin feature.
+ *    - ⏸ (Pause) : Stops the feature.
+ *    - 🔄 (Restart) : Updates graph element & starts the feature.
+ * 
+ * If plugin is running on a page & you switch to another, plugin autostops.
+ * 
+ * Difference between Play & Restart: 
+ * 
+ * When played for first time, plugin stores the ids of all current graph live 
+ * elements, to not have to fetch them each time there is an update. 
+ * When Pause is clicked, live update feature is disabled but the stored ids stay, 
+ * then id storage is not done when you click on Play. 
+ * If the graph changes during the pause with live elements added or removed, you 
+ * should click on Restart to perform the ids storage in the current graph & avoid 
+ * troubles.
+ */
 Draw.loadPlugin(
   function(ui) {
     // stores live properties values
@@ -162,7 +214,7 @@ Draw.loadPlugin(
         live.ids = findLiveElementsIds(graph);
         live.nodes = storeLiveElements(graph, live.ids);
         live.isInit = true;
-        live.timeout = +(root.firstChild.getAttribute(live.refresh) + "000");
+        live.timeout = (+(root.firstChild.getAttribute(live.refresh) + "000")) || 10000;
         live.graphId = ui.currentPage.node.id;
       }
 
@@ -233,7 +285,7 @@ Draw.loadPlugin(
         );
       }
       else {
-        log("live update plugin: Page changed, plugin stopped");
+        log("Page changed, plugin stopped");
         resetScheduleUpdate();
       }
     }
