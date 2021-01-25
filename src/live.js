5348e36a55a49d56a9fbd3893117419ed9a0bbb5
Totone <contact@totone.dev>
2021-01-25 17:22:42 +0100

    feat: plugin can now fetch js objects from an api

--- src/live.js
+++ src/live.js
@@ -20,7 +20,7 @@
  *        Example: "live.property.fillOpacity" updates "Fill Opacity" element 
  *        property.
  * 
- * The value for each live propertiy is the url used to get the requested resource.
+ * The value for each live property is the url used to get the requested resource.
  * The value can be an absolute path (ex: https://my.api/path/to/resource) or a 
  * relative path: in this case, stored value is concatenated with live.api prefix 
  * to build the request full url.
@@ -59,6 +59,9 @@ Draw.loadPlugin(
       refresh: "live.refresh",
       style: "live.style",
       text: "live.text",
+      data: "live.data",
+      source: "live.source",
+      sourceDefaultValue: "hits.hits[0]._source",
       property: {
         prefix: "live.property.",
         getName: (fullPropName) => fullPropName.slice("live.property.".length)
@@ -66,10 +69,86 @@ Draw.loadPlugin(
       nodes: [],
       timeout: 0,
       isInit: false,
-      graphPageId: ""
+      graphPageId: "",
+      mxUtilsRequestErrorMsg: "{\"status\": \"error\"}"
     };
 
     addLiveUpdatePalette();
+
+
+    /** Performs an update process */
+    function doUpdate() {
+      clearThread(live.thread);
+      const graph = ui.editor.getGraphXml();
+      const root = graph.firstChild;
+      
+      // when inits or restarts
+      if(!live.isInit) {
+        live.timeout = (+(root.firstChild.getAttribute(live.refresh) + "000")) || 10000;
+        live.graphPageId = ui.currentPage.node.id;
+        live.nodes = findLiveNodes(graph);
+        live.isInit = true;
+      }
+
+      // initiates the xml doc to perform the updates & the array which stores live objects data
+      const xmlUpdatesDoc = mxUtils.createXmlDocument();
+      const updatesList = xmlUpdatesDoc.createElement("updates");
+      const liveObjects = [];
+  
+      for(const {node, id} of live.nodes) {
+        // creates an update node for each targetted live node
+        const updateNode = xmlUpdatesDoc.createElement("update");
+        updateNode.setAttribute("id", id);
+
+        for(const {name: attrName, value: attrValue} of node.attributes) {          
+          // targets all live properties
+          if(attrName.startsWith("live.")) {
+            if(attrName !== live.data && attrName !== live.source) {
+              const updateOptions = {
+                node,
+                attrName,
+                attrValue,
+                apiPrefix: root.firstChild.getAttribute(live.api)
+              };
+  
+              if(attrValue.startsWith("=")) { // case: live object
+                storeLiveObjectsData(liveObjects, updateOptions);
+              } else { // case: live value
+                fetchLiveValue(updateNode, updateOptions);
+              }
+            }
+          }
+        }
+        updatesList.appendChild(updateNode);
+      }
+
+      // performs updates for each live object previously stored
+      for(const liveObjectData of liveObjects) {
+        computeLiveObject(
+        liveObjectData, 
+        updatesList, 
+        xmlUpdatesDoc.createElement
+        );
+      }
+
+      // appends "updates" node to the new doc & updates diagram with it
+      if(ui.currentPage.node.id === live.graphPageId) {
+
+        xmlUpdatesDoc.appendChild(updatesList);
+        ui.updateDiagram(
+          mxUtils.getXml(xmlUpdatesDoc)
+        );
+
+        live.thread = setTimeout(
+          doUpdate,
+          live.timeout
+        );
+      }
+      else {
+        log("Page changed, plugin stopped");
+        resetScheduleUpdate();
+      }
+    }
 
     /** Adds a new palette with buttons to handle the live state in the toolbar */
     function addLiveUpdatePalette() {
@@ -128,7 +207,7 @@ Draw.loadPlugin(
     }
 
     /** Resets live update parameters */
-    function resetScheduleUpdate(isRestart = false) {
+    function resetScheduleUpdate() {
       live.ids = [];
       live.nodes = [];
       live.isInit = false;
@@ -144,110 +223,28 @@ Draw.loadPlugin(
       startScheduleUpdate();
     }
 
-    /** Performs an update process */
-    function doUpdate() {
-      clearThread(live.thread);
-      const graph = ui.editor.getGraphXml();
-      const root = graph.firstChild;
-
-      // when inits or restarts
-      if(!live.isInit) {
-        live.timeout = (+(root.firstChild.getAttribute(live.refresh) + "000")) || 10000;
-        live.graphPageId = ui.currentPage.node.id;
-        live.nodes = findLiveNodes(graph);
-        live.isInit = true;
-      }
-
-      // initiates the xml doc to perform the updates
-      const xmlUpdatesDoc = mxUtils.createXmlDocument();
-      const updatesList = xmlUpdatesDoc.createElement("updates");
-  
-      for(const {node, id} of live.nodes) {
-        let updateNode = xmlUpdatesDoc.createElement("update");
-        updateNode.setAttribute("id", id);
-
-        for(const attribute of node.attributes) {
-          const {name: attrName, value: attrValue} = attribute;
-          
-          // targets all live properties
-          if(attrName.startsWith("live.")) {
-            // case: live value
-            const updateOptions = {
-              liveAttrName: attrName,
-              url: computeRequest(attrValue, root.firstChild.getAttribute(live.api)),
-              style: node.childNodes[0].getAttribute("style")
-            };
-            updateNode = fetchLiveValue(updateNode, updateOptions);
-          }
-        }
-        updatesList.appendChild(updateNode);
-      }
-
-      // appends "updates" node to the new doc & updates diagram with it
-      if(ui.currentPage.node.id === live.graphPageId) {
-
-        xmlUpdatesDoc.appendChild(updatesList);
-        ui.updateDiagram(
-          mxUtils.getXml(xmlUpdatesDoc)
-        );
-
-        live.thread = setTimeout(
-          doUpdate,
-          live.timeout
-        );
-      }
-      else {
-        log("Page changed, plugin stopped");
-        resetScheduleUpdate();
-      }
-    }
-
     /** Fecthes value from distant api for current attribute */
     function fetchLiveValue(currentLiveNode, options) {
-      let {liveAttrName, url, style} = options;
+      const {node, attrName, attrValue, apiPrefix} = options;
+      const style = node.childNodes[0].getAttribute("style");
+      
+      const url = computeRequest(attrValue, apiPrefix);
+      const liveValue = computeApiResponse(url, true);
 
-      try {
-        const apiResponse = mxUtils.load(url);
-        
-        if(apiResponse) {
-          let parsedResponse = parseApiResponse(apiResponse);
-
-          if(liveAttrName === live.text) {
-            currentLiveNode.setAttribute(
-              "value", 
-              `<object label="${parsedResponse}"/>`
-            );
-          } else if (liveAttrName === live.style) {
-            currentLiveNode.setAttribute(
-              "style", 
-              parsedResponse
-            );
-          } else {
-            style = mxUtils.setStyle(
-              style, 
-              live.property.getName(liveAttrName), 
-              parsedResponse
-            );
-            currentLiveNode.setAttribute("style", style);
-          }
-          return currentLiveNode;
-        }
-      }
-      catch(e) {
-        log(`Error while fetching data from ${requestUrl}: ${e}`);
-      }
+      fillUpdateNode(
+        currentLiveNode, 
+        attrName, 
+        liveValue, 
+        style
+      );
     }
 
-    /** Parses received response from distant API */
-    function parseApiResponse(response) {
-      return response.getText().replace(/"/g, "").trim();
-    } 
-
     /** Computes the request to call the API according to the given uri */
-    function computeRequest(url, liveApi) {
-      return url.startsWith("http") ? url     // absolute path
-      : url.startsWith("/") ? liveApi + url   // relative path
-      : null;                                 // error
+    function computeRequest(url, rootApi) {
+      const request = url.startsWith("http") ? url  // absolute path
+      : url.startsWith("/") ? rootApi + url         // relative path
+      : null;                                       // error
+      return request;
     }
 
     /** Checks recursively in xml tree & stores nodes if they are live ones */
@@ -288,8 +285,142 @@ Draw.loadPlugin(
       return liveNodes;
     }
 
-    function log(text) {
-      console.log("liveUpdate plugin:", text);
+    /** Stores data to update nodes attributes from distant js object */
+    function storeLiveObjectsData(currentList, options) {
+      const {node, attrName, attrValue, apiPrefix} = options;
+      const url = node.hasAttribute(live.data) 
+      ? computeRequest(
+        node.getAttribute(live.data),
+        apiPrefix
+      ) : apiPrefix;
+
+      const source = node.hasAttribute(live.source) 
+      ? node.getAttribute(live.source)
+      : live.sourceDefaultValue; 
+      const nodeId = node.getAttribute("id");
+
+      const newListenerAttr = {
+        attrName,
+        attrValue: attrValue.slice(1)
+      };
+
+      const newListener = {
+        id: nodeId,
+        style: node.childNodes[0].getAttribute("style"),
+        attrs: [
+          newListenerAttr
+        ]
+      };
+
+      const idInList = currentList.findIndex(
+        objectData => objectData.url === url
+      );
+
+      if(idInList === -1) {
+        const newObjectData = {
+          url,
+          source,
+          listeners: [
+            newListener
+          ]
+        };
+        currentList.push(newObjectData);
+      } else {
+        const listenerId = currentList[idInList].listeners.findIndex(
+          listener => listener.id === nodeId
+        );
+
+        if(listenerId === -1) {
+          currentList[idInList].listeners.push(newListener);
+        } else {
+          currentList[idInList].listeners[listenerId].attrs.push(newListenerAttr);
+        }
+      }
+    }
+
+    /** Computes values for each object attributes of a live node */
+    function computeLiveObject(object, updates, createNode) {
+      const {url, source, listeners} = object;
+      const liveRawObject = computeApiResponse(url, false);
+
+      const dataSource = new Function(
+        "responseRoot", 
+        "return responseRoot." + source
+      )(liveRawObject);
+
+      for(const listener of listeners) {
+        const existingNode = mxUtils.findNode(
+          updates,
+          "id",
+          listener.id
+        );
+        const update = {};
+        update.node = existingNode ? existingNode : createNode("update");
+        update.isNew = existingNode ? true : false;
+
+        for(const {attrName, attrValue} of listener.attrs) {
+          const attrUpdatedValue = new Function(
+            "data",
+            attrValue
+          )(dataSource);
+
+          fillUpdateNode(
+            update.node, 
+            attrName, 
+            attrUpdatedValue,
+            listener.style
+          );
+        }
+        if(!update.isNew) updates.appendChild(update.node);
+      }
+    }
+
+    /** Fills an update node */
+    function fillUpdateNode(updateNode, attrName, attrValue, inputStyle) {
+      if(attrName === live.text) {
+        updateNode.setAttribute(
+          "value", 
+          `<object label="${attrValue}"/>`
+        );
+      } else if (attrName === live.style) {
+        updateNode.setAttribute(
+          "style", 
+          attrValue
+        );
+      } else {
+        const style = mxUtils.setStyle(
+          updateNode.hasAttribute("style") ? updateNode.getAttribute("style") : inputStyle,
+          live.property.getName(attrName), 
+          attrValue
+        );
+        updateNode.setAttribute("style", style);
+      }
+    }
+
+    /** Sends request to distant api & parses response in the corproper form */
+    function computeApiResponse(url, isString) {
+      function loadDataFromApi(url) {
+        return mxUtils.load(url);
+      }
+
+      /** Parses received response from distant API */
+      function parseApiResponse(rawResponse, isString = true) {
+        const parsedResponse = rawResponse.getText();
+
+        if(isString) {
+          return parsedResponse.replace(/"/g, "").trim();
+        } else {
+          return JSON.parse(parsedResponse);
+        }
+      } 
+
+      const rawResponse = loadDataFromApi(url);
+      const parsedResponse = parseApiResponse(rawResponse, isString);
+      return parsedResponse;
+    }
+
+    function log(...text) {
+      console.log("liveUpdate plugin:", ...text);
     }
   }
 );
